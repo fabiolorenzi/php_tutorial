b41276a2ee79e32df5c3e8575818dcd0c8c8de66
fabiolorenzi <fabiolorenzi@gmail.com>
2021-09-25 10:15:28 +0100

    constructors

--- classesAndObjects.php
+++ classesAndObjects.php
@@ -25,6 +25,41 @@
         $book2->pages = 700;
 
         echo $book2->author;
+
+        echo("<br />");
+
+        class Movie {
+            var $title;
+            var $author;
+            var $vote;
+
+            function __construct($code) {
+                echo("$code inserted <br />");
+            }
+        };
+
+        $movie1 = new Movie(0123);
+
+        echo("<br />");
+        echo("How to use contructor to create faster a new product");
+        echo("<br />");
+        echo("<br />");
+
+        class Magazine {
+            var $title;
+            var $writer;
+            var $price;
+
+            function __construct($aTitle, $aWriter, $aPrice) {
+                $this->title = $aTitle;
+                $this->writer = $aWriter;
+                $this->price = $aPrice;
+
+            }
+        };
+
+        $mag1 = new Magazine("title1", "Jefferson", 4.99);
+        echo $mag1->price;
     ?>
 </body>
 </html>
